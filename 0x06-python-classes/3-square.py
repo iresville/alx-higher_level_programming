b7124f418b27e0fc5fb845ea4b3f3c60db6eac42
Jo <iresgh1@gmail.com>
2023-05-31 21:22:50 +0000

    create class instance and return square method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """ A Class that defines a square"""
-  
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
