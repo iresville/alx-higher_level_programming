7b1acd151e85f74b0abddf1e5194f12c261e028f
Jo <iresgh1@gmail.com>
2023-05-31 18:40:11 +0000

    square_area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""create Class Square"""
+
+
+class Square:
+    """create instance of a Class"""
+    def __init__(self, size=0):
+        self.__size = size
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.size ** 2
