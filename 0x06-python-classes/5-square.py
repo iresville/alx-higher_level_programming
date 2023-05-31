047770c40c16f5a7f8b6b76dad316fa125ae2232
Jo <iresgh1@gmail.com>
2023-05-31 22:50:12 +0000

    setter and position methods

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,15 +5,16 @@
 class Square:
     """A class that defines a square"""
     def __init__(self, size=0):
-        self.__size = 0
-        self.size = size
+		self.__size = size
 
     @property
     def size(self):
+        """Get size of square matrix"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set size of square matrix"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -22,4 +23,15 @@ class Square:
             self.__size = value
 
     def area(self):
+        """returns area of square matrix"""
         return self.__size ** 2
+
+    def my_print(self):
+        """print the sqaure matrix using '#' notation"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
