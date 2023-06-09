d6407c0ae61344621917116862b8d128f57db120
iresville <iresgh1@gmail.com>
2023-06-09 12:49:27 +0000

    update tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,81 +1,35 @@
 #!/usr/bin/python3
-class Square:
-  def ___init__(self, size):
-    """initialize a new Square"""
-    self.___size = size
-    if not isinstance(size, int):
-      raise TypeError("size must be an integer")
-    elif size < 0;
-      raise ValueError(size must be >= 0") 
-    
-    @property
-    def size(self)
+"""Defines a matrix division function."""
 
-    @size.setter
-    def size(self, value):
-      if not isinstance(value, int)
-      raise TypeError("size must be an integer")
-    elif size < 0:
-      raise ValueError("size must be >=0")
-    self.___size = value 
-    
-    def area(self)
-    """Return the current area of the square size x size"""
-    return (self.__size*self.__size)
-    
-class Car:
-  def ___init___(self, brand, model, color,specs):
-  """initilaize a new class Car"""
-  self.brand = brand
-  self.model = model
-  self.color = color 
-  self.specs = specs
-  if not isinstance(brand, char):
-    raise TypeError("brand must be a char")
-  elif char != 0:
-    raise ValueError("brand must not be an integer")
-    
-    @property
-    def color(self)
-    """Get/set the current color of the Car"""
-    return (self.color)
-    
-    def specs(self)
-    """Return the specs of a class Car as brand x model"""
-    return (self.brand*self.model)
-    
-      
-class Circle:
-  def ___init___(self, size , format)
-  """Initilaise a new Circle"""
-  self.size = size
-  self.format = format
-  if not isinstance(size, int):
-    raise TypeError("size must be an integer")
-  elif size < 0:
-    raise ValueError("size must be >= 0")
 
-def area(self):
-  return (self.size Squared)
+def matrix_divided(matrix, div):
+    """Divide all elements of a matrix.
 
-@property
-def size(self)
-return (self.size)
-  
-class Family:
-  def ___init___(self, name, age , weight )
-  self.name = name 
-  self.age = age 
-  self.weight = weight
-if not isinstance(name, char)
-raise TypeError("Name must be a char")
-if not isinstance(age, int)
-raise ValueError("size must be >=0")
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+        A new matrix representing the result of the division.
+    """
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
-@property
-def weight(self)
-"""Get/set the weight of the family object"""
-retutn (self.weight)
-return(self.weight)
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
 
-@size.setter
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
